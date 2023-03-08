ca2574b2de30ae51cc9d5f5d0b91f97cc72069bc
matias.semelman <matias.semelman@devify.it>
2023-03-08 16:02:12 -0300

    Push

---
+++ assets/icons/Vector.tsx
@@ -0,0 +1,20 @@
+export const Vector = () => {
+  return (
+    <svg
+      width="14"
+      height="8"
+      viewBox="0 0 14 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.9405 1.71249L8.05055 6.60249C7.47305 7.17999 6.52805 7.17999 5.95055 6.60249L1.06055 1.71249"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
